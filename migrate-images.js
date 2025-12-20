e9ad5fd3cd41fc017e85fea09bc07757a27a8931
seouldish-glitch <seouldish@gmail.com>
2025-12-20 18:39:00 +0530

    Add files via upload

--- migrate-images.js
+++ migrate-images.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 async function migrateImages() {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("✅ Connected to MongoDB");
 
         const Product = mongoose.model("Product", new mongoose.Schema({}, { strict: false }));
